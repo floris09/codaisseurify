99a673daf0e45cdf5c09371b9753b02a4cfc1af2
floris09 <florismeininger@gmail.com>
2017-10-12 16:03:01 +0200

    Write tests for model validations

--- spec/models/artist_spec.rb
+++ spec/models/artist_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Artist, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_length_of(:bio).is_at_most(500) }
+  end
 end
