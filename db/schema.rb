625f907469b27f12e3162ed29f6f1acc1d49978f
floris09 <florismeininger@gmail.com>
2017-10-13 09:30:09 +0200

    Generate Photo model and add photo-artist associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171012124103) do
+ActiveRecord::Schema.define(version: 20171013072741) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20171012124103) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "photos", force: :cascade do |t|
+    t.bigint "artist_id"
+    t.string "image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["artist_id"], name: "index_photos_on_artist_id"
+  end
+
   create_table "songs", force: :cascade do |t|
     t.string "name"
     t.bigint "artist_id"
@@ -30,5 +38,6 @@ ActiveRecord::Schema.define(version: 20171012124103) do
     t.index ["artist_id"], name: "index_songs_on_artist_id"
   end
 
+  add_foreign_key "photos", "artists"
   add_foreign_key "songs", "artists"
 end
